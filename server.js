c139123de5eb6296d751dc01c7b8ddcd10619eb7
RMSanchez <raulsg93@gmail.com>
2018-07-28 16:21:03 -0500

    home page built, survey page started, and server page listening

--- server.js
+++ server.js
@@ -6,3 +6,21 @@ var app = express();
 //building a server here
 
 var PORT= process.env.PORT || 3000;
+
+//routes
+//=============================
+app.get("/home.html", function(req, res){
+    //do something
+
+});
+app.get("/servey.html", function(req, res){
+    //do something
+
+});
+
+
+//listener
+//=============================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
